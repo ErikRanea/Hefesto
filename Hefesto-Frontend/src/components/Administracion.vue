48e45420050911ba8aeaca6fdfd4eff3dd3a63af
unknown <2gdaw08@ARRIAGAINFO.LOCAL>
2025-01-21 13:13:00 +0100

    maquinas popup

--- Hefesto-Frontend/src/components/Administracion.vue
+++ Hefesto-Frontend/src/components/Administracion.vue
@@ -1,76 +1,116 @@
 <template>
-<div class="container-fluid h-100">
-    <div class="row">
-        <div class="col-md-6 h-100">
-            <button class="glassmorphic-card mb-4">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="card-body text-center">
-                        <h2>Usuarios</h2>
+    <div class="container-fluid h-100">
+        <div class="row">
+            <div class="col-md-6 h-100">
+                <button class="glass-card mb-4">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-body text-center">
+                            <h2 class="card-text">Usuarios</h2>
+                        </div>
+                        <div class="card-body text-center">
+                            <img src="../assets/images/icons/usuarios.svg">
+                        </div>
                     </div>
-                    <div class="card-body text-center">
-                        <img src="../assets/images/icons/usuarios.svg">
+                </button>
+                <button class="glass-card mb-4">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-body text-center">
+                            <h2 class="card-text">Campuses</h2>
+                        </div>
+                        <div class="card-body text-center">
+                            <img src="../assets/images/icons/campuses.svg">
+                        </div>
                     </div>
-                </div>
-            </button>
-            <button class="glassmorphic-card mb-4">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="card-body text-center">
-                        <h2>Campuses</h2>
+                </button>
+            </div>
+            <div class="col-md-6">
+                <button class="glass-card mb-4">
+                    <div class="justify-content-center align-items-center text-center">
+                        <div class="card-body justify-content-center align-items-center mb-3">
+                            <h2 class="card-text">Secciones</h2>
+                        </div>
+                        <div class="card-body-2">
+                            <img src="../assets/images/icons/secciones.svg">
+                        </div>
                     </div>
-                    <div class="card-body text-center">
-                        <img src="../assets/images/icons/campuses.svg">
-                    </div>
-                </div>
-            </button>
+                </button>
+            </div>
         </div>
-        <div class="col-md-6">
-            <button class="glassmorphic-card mb-4">
-                <div class="justify-content-center align-items-center text-center">
-                    <div class="card-body justify-content-center align-items-center mb-3">
-                        <h2>Secciones</h2>
+        <div class="row">
+            <div class="col">
+                <button class="glass-card mb-4" @click="openPopup('maquinas')">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-body text-center">
+                            <h2 class="card-text">Maquinas</h2>
+                        </div>
+                        <div class="card-body text-center">
+                            <img width="200" src="../assets/images/icons/maquinas.svg">
+                        </div>
                     </div>
-                    <div class="card-body-2">
-                        <img src="../assets/images/icons/secciones.svg">
-                    </div>
-                </div>
-            </button>
+                </button>
+            </div>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col">
-            <button class="glassmorphic-card mb-4">
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="card-body text-center">
-                        <h2>Maquinas</h2>
-                    </div>
-                    <div class="card-body text-center">
-                        <img width="200" src="../assets/images/icons/maquinas.svg">
-                    </div>
-                </div>
-            </button>
-        </div>
-    </div>
-
-</div>
-
+    <GlassmorphicPopup
+            :visible="popupVisible"
+            :title="popupTitle"
+            :subtitle="popupSubtitle"
+            :closeButtonText="popupCloseButtonText"
+    >
+    </GlassmorphicPopup>
 </template>
 
-<script>
-export default {
-    name: 'Administracion',
-}
+<script setup>
+    import GlassmorphicPopup from './GlassmorphicPopup.vue';
+    import 'bootstrap/dist/css/bootstrap.css';
+    import 'bootstrap/dist/js/bootstrap.js';
+    import { ref, onMounted, computed } from 'vue';
+    import axios from 'axios';
+
+    const popupVisible = ref(false);
+    const popupType = ref(null)
+    const popupTitle = ref(null)
+    const popupSubtitle = ref(null)
+    const popupCloseButtonText = ref(null)
+
+    const openPopup = (type) => {
+        popupVisible.value = true;
+        popupType.value = type;
+
+        if (type === 'maquinas') {
+            popupTitle.value = 'Maquinas'
+            popupSubtitle.value = 'Listado de maquinas'
+            popupCloseButtonText.value = 'Cerrar'
+        }
+    };
+
+    const closePopup = () => {
+        popupVisible.value = false;
+    };
 </script>
 
 <style scoped>
 
-    .glassmorphic-card {
-        background: rgba(255, 255, 255, 0.7);
-        backdrop-filter: blur(8px);
-        border: 1px solid rgba(255, 255, 255, 0.05);
-        box-shadow: 0 4px 24px 0 rgba(0, 0, 0, 0.3);
+    .glass-card {
+        background: rgba(255, 255, 255, 0.2);
         border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(5px);
+        -webkit-backdrop-filter: blur(5px);
+        border: 1px solid rgba(255, 255, 255, 0.3);
         width: 100%;
+        height: auto;
+        cursor: pointer;
+        transition: transform 0.2s ease-in-out;
+    }
+
+    .glass-card:hover {
+        transform: scale(1.05);
+    }
+
+    .card-text {
+        margin-top: 0;
+        color: #333;
     }
 
     .card-body {
