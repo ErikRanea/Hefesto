ed5499e8ca2834266fc97faae5f7458404bda503
unknown <2gdaw08@arriagainfo.local>
2025-01-22 08:43:42 +0100

    administracion mantenimiento

--- Hefesto-Frontend/src/components/Administracion.vue
+++ Hefesto-Frontend/src/components/Administracion.vue
@@ -49,14 +49,42 @@
                     </div>
                 </button>
             </div>
+            <div class="col">
+                <button class="glass-card mb-4">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="card-body text-center">
+                            <h2 class="card-text">Mantenimiento</h2>
+                        </div>
+                        <div class="card-body text-center">
+                            <img width="177" src="../assets/images/icons/mantenimiento.svg">
+                        </div>
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
     <GlassmorphicPopup
-            :visible="popupVisible"
-            :title="popupTitle"
-            :subtitle="popupSubtitle"
-            :closeButtonText="popupCloseButtonText"
+        :visible="popupVisible"
+        :title="popupTitle"
+        :subtitle="popupSubtitle"
+        :closeButtonText="popupCloseButtonText"
+        :actionButtonText="popupActionButtonText"
+        :imageUrl="userImagePath"
+        @close="closePopup"
+        @action="handleAction"
+        @image-changed="updateProfileImage"
     >
+        <template #popup-content v-if="popupType !== 'perfil'">
+            <div v-if="popupType === 'contraseña'">
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" class="form-control" placeholder="Tu contraseña">
+            </div>
+            <div v-else-if="popupType === 'fondo'">
+                <textarea id="descripcion" class="form-control" placeholder="Descripción"></textarea>
+            </div>
+            <div v-else-if="popupType === 'maquinas'">
+            </div>
+        </template>
     </GlassmorphicPopup>
 </template>
 
@@ -66,27 +94,113 @@
     import 'bootstrap/dist/js/bootstrap.js';
     import { ref, onMounted, computed } from 'vue';
     import axios from 'axios';
-
+    
+    const API_AUTH_URL = import.meta.env.VITE_API_AUTH_URL;
+    const ME_URL = `${API_AUTH_URL}/auth/me`;
+    const UPDATE_PROFILE_IMAGE_URL = `${API_AUTH_URL}/auth/update-profile-image`;
+    
     const popupVisible = ref(false);
     const popupType = ref(null)
     const popupTitle = ref(null)
     const popupSubtitle = ref(null)
     const popupCloseButtonText = ref(null)
-
+    const popupActionButtonText = ref(null)
+    const userPicture = ref(null);
+    const userName = ref(null);
+    const userLastName = ref(null);
+    
     const openPopup = (type) => {
         popupVisible.value = true;
         popupType.value = type;
-
-        if (type === 'maquinas') {
+    
+        if (type === 'perfil'){
+            popupTitle.value = 'Editar perfil'
+            popupSubtitle.value = 'Edita tu perfil'
+            popupCloseButtonText.value = 'Cerrar'
+            popupActionButtonText.value = 'Guardar'
+        }
+        else if(type === 'contraseña'){
+            popupTitle.value = 'Cambiar contraseña'
+            popupSubtitle.value = 'Introduce tu nueva contraseña'
+            popupCloseButtonText.value = 'Cerrar'
+            popupActionButtonText.value = 'Guardar'
+        }
+        else if (type === 'fondo'){
+            popupTitle.value = 'Cambiar fondo'
+            popupSubtitle.value = 'Añade la descripción de tu fondo'
+            popupCloseButtonText.value = 'Cerrar'
+            popupActionButtonText.value = 'Aceptar'
+        }
+        else if (type === 'maquinas'){
             popupTitle.value = 'Maquinas'
             popupSubtitle.value = 'Listado de maquinas'
             popupCloseButtonText.value = 'Cerrar'
+            popupActionButtonText.value = 'Aceptar'
         }
     };
-
+    
     const closePopup = () => {
         popupVisible.value = false;
     };
+    
+    
+    const handleAction = async() => {
+        if(popupType.value === 'perfil'){
+            // Handle profile picture update logic here
+        } else{
+            alert(`Action clicked on ${popupType.value}`)
+        }
+        closePopup();
+    };
+    
+        const updateProfileImage = async (imageURL) => {
+            if (!imageURL) return;
+            const token = localStorage.getItem('token');
+            try {
+            const formData = new FormData();
+            const response = await fetch(imageURL);
+                const blob = await response.blob();
+    
+            const file = new File([blob], 'profile-image', {type: 'image/*'});
+                formData.append('profile_image', file);
+                const response_upload = await axios.post(UPDATE_PROFILE_IMAGE_URL, formData,{
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                            'Content-Type': 'multipart/form-data',
+                        },
+                    }
+            );
+    
+                userPicture.value = response_upload.data.foto_perfil
+                alert('Profile image uploaded successfully!');
+            } catch (error) {
+            console.error('Error updating profile image:', error);
+            alert('Failed to upload profile image!');
+            }
+        };
+    
+    
+        const userImagePath = computed(() => {
+            return userPicture.value ? `../src/assets/images/userpicture/${userPicture.value}` : null;
+        });
+    
+    onMounted(async () => {
+        const token = localStorage.getItem('token');
+        if (token) {
+        try {
+            const response = await axios.get(ME_URL, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            });
+            userName.value = response.data.name;
+            userLastName.value = response.data.primer_apellido;
+            userPicture.value = response.data.foto_perfil;
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+        }
+    });
 </script>
 
 <style scoped>
@@ -118,7 +232,7 @@
     }
 
     .card-body-2 {
-        padding: 73px;
+        padding: 77px;
     }
 
 </style>
