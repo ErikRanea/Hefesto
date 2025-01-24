238bc31f2c00d2789784a37626b89dccfce72713
IbaiSaenzDeBuruaga <ibai.saenzdeburuaga@ikasle.egibide.org>
2025-01-24 11:39:20 +0100

    crud usuarios funcional

--- Hefesto-Frontend/src/main.js
+++ Hefesto-Frontend/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import './style.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(Toast)
+app.mount('#app')
