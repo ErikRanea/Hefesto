e8235389cca86b754e992e401b13362aa0a7e72d
unknown <2gdaw08@ARRIAGAINFO.LOCAL>
2025-01-24 12:14:01 +0100

    maquinasComponent View

--- Hefesto-Frontend/src/components/Administracion.vue
+++ Hefesto-Frontend/src/components/Administracion.vue
@@ -1,161 +1,139 @@
 <template>
     <div class="container-fluid h-100">
-        <div class="row">
-            <div class="col-md-6 h-100">
-                <div class="d-flex flex-column h-100">
-                    <button class="glass-card mb-4">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="card-body text-center">
-                                <h2 class="card-text">Usuarios</h2>
+       <div v-if="!activeComponent">
+           <div class="row">
+             <div class="col-md-6 h-100">
+                    <div class="d-flex flex-column h-100">
+                        <button class="glass-card mb-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="card-body text-center">
+                                    <h2 class="card-text">Usuarios</h2>
+                                </div>
+                                <div class="card-body text-center">
+                                    <img src="../assets/images/icons/usuarios.svg">
+                                </div>
                             </div>
-                            <div class="card-body text-center">
-                                <img src="../assets/images/icons/usuarios.svg">
+                        </button>
+                        <button class="glass-card mb-4">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="card-body text-center">
+                                    <h2 class="card-text">Campuses</h2>
+                                </div>
+                                <div class="card-body text-center">
+                                    <img src="../assets/images/icons/campuses.svg">
+                                </div>
                             </div>
-                        </div>
-                    </button>
-                    <button class="glass-card mb-4">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="card-body text-center">
-                                <h2 class="card-text">Campuses</h2>
+                        </button>
+                        <button class="glass-card">
+                            <div class="d-flex justify-content-center align-items-center text-center">
+                                <div class="card-body text-center">
+                                    <h2 class="card-text">Secciones</h2>
+                                </div>
+                                <div class="card-body text-center">
+                                    <img width="144" src="../assets/images/icons/secciones.svg">
+                                </div>
                             </div>
+                        </button>
+                    </div>
+                </div>
+            <div class="col-md-6">
+                    <button class="glass-card mb-4" @click="openComponent('maquinas')">
+                        <div class="justify-content-between align-items-center mb-3">
                             <div class="card-body text-center">
-                                <img src="../assets/images/icons/campuses.svg">
+                                <h2 class="card-text">Maquinas</h2>
+                            </div>
+                            <div class="card-body-2 text-center">
+                                <img width="250" src="../assets/images/icons/maquinas.svg">
                             </div>
                         </div>
                     </button>
                     <button class="glass-card">
-                        <div class="d-flex justify-content-center align-items-center text-center">
+                        <div class="d-flex justify-content-between align-items-center">
                             <div class="card-body text-center">
-                                <h2 class="card-text">Secciones</h2>
+                                <h2 class="card-text">Mantenimientos preventivos</h2>
                             </div>
                             <div class="card-body text-center">
-                                <img width="144" src="../assets/images/icons/secciones.svg">
+                                <img width="180" src="../assets/images/icons/mantenimiento.svg">
                             </div>
                         </div>
                     </button>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <button class="glass-card mb-4" @click="openPopup('maquinas')">
-                    <div class="justify-content-between align-items-center mb-3">
-                        <div class="card-body text-center">
-                            <h2 class="card-text">Maquinas</h2>
-                        </div>
-                        <div class="card-body-2 text-center">
-                            <img width="250" src="../assets/images/icons/maquinas.svg">
-                        </div>
-                    </div>
-                </button>
-                <button class="glass-card">
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="card-body text-center">
-                            <h2 class="card-text">Mantenimientos preventivos</h2>
-                        </div>
-                        <div class="card-body text-center">
-                            <img width="180" src="../assets/images/icons/mantenimiento.svg">
-                        </div>
-                    </div>
-                </button>
-            </div>
-        </div>
-    </div>
-     <MaquinasPopup
-        ref="maquinasPopupRef"
-        :visible="popupVisible"
-        :title="popupTitle"
-        :subtitle="popupSubtitle"
-        :closeButtonText="popupCloseButtonText"
-        :actionButtonText="popupActionButtonText"
-        :token="token"
-        @close-popup="closePopup"
-        />
+                 </div>
+               </div>
+       </div>
+       <component  v-else  :is="activeComponent" :token="token" @close-component="closeComponent"/>
+   </div>
 </template>
 
 <script setup>
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted, markRaw } from 'vue';
 import axios from 'axios';
-import MaquinasPopup from './MaquinasPopup.vue';
-const maquinasPopupRef = ref(null);
+import MaquinasComponent from './MaquinasComponent.vue';
 
 const API_AUTH_URL = import.meta.env.VITE_API_AUTH_URL;
 const ME_URL = `${API_AUTH_URL}/auth/me`;
+const activeComponent = ref(null);
+const token = ref(null);
 
-// --- Variables del Popup ---
-const popupVisible = ref(false);
-const popupType = ref(null);
-const popupTitle = ref(null);
-const popupSubtitle = ref(null);
-const popupCloseButtonText = ref(null);
-const popupActionButtonText = ref(null);
-const token = ref(null)
-
-
-// --- Funciones del Popup ---
-const openPopup = (type) => {
-    popupVisible.value = true;
-    popupType.value = type;
-
-    if (type === 'maquinas'){
-        popupTitle.value = 'Maquinas';
-        popupSubtitle.value = 'Listado maquinas';
-        popupCloseButtonText.value = 'Cerrar';
-        popupActionButtonText.value = 'Aceptar';
-        maquinasPopupRef.value.fetchData();
-    }
+const openComponent = (type) => {
+ if (type === 'maquinas') {
+   activeComponent.value = markRaw(MaquinasComponent);
+ }
 };
 
-const closePopup = () => {
-    popupVisible.value = false;
-};
+const closeComponent = () => {
+  activeComponent.value = null;
+}
 
 onMounted(async () => {
-    token.value = localStorage.getItem('token');
-    if (token.value) {
-        try {
-            const response = await axios.get(ME_URL, {
-                headers: {
-                    Authorization: `Bearer ${token.value}`,
-                },
-            });
-        } catch (error) {
-           console.error('Error fetching data:', error);
-        }
-    }
+ token.value = localStorage.getItem('token');
+ if (token.value) {
+   try {
+     await axios.get(ME_URL, {
+       headers: {
+         Authorization: `Bearer ${token.value}`,
+       },
+     });
+   } catch (error) {
+     console.error('Error fetching data:', error);
+   }
+ }
 });
+
+defineExpose({openComponent})
 </script>
 
 <style scoped>
 
-    .glass-card {
-        background: rgba(255, 255, 255, 0.2);
-        border-radius: 16px;
-        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-        backdrop-filter: blur(5px);
-        -webkit-backdrop-filter: blur(5px);
-        border: 1px solid rgba(255, 255, 255, 0.3);
-        width: 100%;
-        height: auto;
-        cursor: pointer;
-        transition: transform 0.2s ease-in-out;
-    }
+.glass-card {
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    width: 100%;
+    height: auto;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+}
 
-    .glass-card:hover {
-        transform: scale(1.05);
-    }
+.glass-card:hover {
+    transform: scale(1.05);
+}
 
-    .card-text {
-        margin-top: 0;
-        color: #333;
-    }
+.card-text {
+    margin-top: 0;
+    color: #333;
+}
 
-    .card-body {
-        padding: 32px;
-    }
+.card-body {
+    padding: 32px;
+}
 
-    .card-body-2 {
-        padding: 120px;
-    }
+.card-body-2 {
+    padding: 120px;
+}
 
 </style>
---
+++ Hefesto-Frontend/src/components/MaquinaDetails.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="glass-card text-center">
+      <div class="row mb-4">
+        <h3>Datos de la Máquina</h3>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <p>Nombre: {{ maquina.nombre_maquina }}</p>
+        </div>
+        <div class="col-6">
+          <p>ID: {{ maquina.id }}</p>
+        </div>
+        <div class="col-6">
+          <p>Número interno: {{ maquina.numero_interno }}</p>
+        </div>
+        <div class="col-6">
+          <p>Tipo: {{ maquina.tipo_maquina }}</p>
+        </div>
+        <div class="col-6">
+          <p>Sección: {{ maquina.id_seccion }}</p>
+        </div>
+        <div class="col-6">
+          <p>Prioridad: {{ maquina.prioridad }}</p>
+        </div>
+      </div>
+    </div>
+    <button @click="$emit('close-details')" class="popup-btn primary">Volver</button>
+ </template>
+ 
+ <script setup>
+
+const props = defineProps({
+  maquina: {
+    type: Object,
+    required: true,
+  },
+ });
+const emit = defineEmits(['close-details'])
+
+</script>
+
+<style scoped>
+
+.glass-card {
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 16px;
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    width: 100%;
+    margin-bottom: 1em;
+    height: auto;
+    cursor: pointer;
+    transition: transform 0.2s ease-in-out;
+    padding: 2em;
+  }
+
+.popup-btn {
+    background: none;
+    border: none;
+    text-align: center;
+    font-size: 1rem;
+    padding: 0.8rem 1.8rem;
+    border-radius: 2rem;
+    cursor: pointer;
+  }
+
+  .popup-btn.primary {
+    background-color: rgba(255, 255, 255, 0.8);
+    color: #000000;
+  }
+
+</style>
